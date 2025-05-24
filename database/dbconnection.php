aa7ae2090843a28d76f9b19d535c5b2031275b51
hmb <hannahbalite178@gmail.com>
2025-05-25 01:27:05 +0800

    Add files via upload

---
+++ database/dbconnection.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection for SQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "wrtclc";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
